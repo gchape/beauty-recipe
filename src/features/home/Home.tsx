4f7716799dcfba13bff49e24cac35568af412ef3
Giorgi Chapidze <chapidzeee.giorgi@gmail.com>
2025-05-20 14:12:07 +0400

    feat: integrate @cloudinary/react and @cloudinary/url-gen; remove local slide images from public/slides

--- src/features/home/Home.tsx
+++ src/features/home/Home.tsx
@@ -1,20 +1,5 @@
-import { Container } from "react-bootstrap";
 import FadeCarousel from "../carousel/FadeCarousel";
-import Products from "../products/Products";
 
 export default function Home() {
-  return (
-    <Container fluid className="mx-0 px-0">
-      <FadeCarousel />
-
-      <div className="d-block text-center my-5">
-        <h3>
-          {/* #e6bfc4 */}
-          ყველაზე <span style={{ color: "#cfa67b" }}>გაყიდვადი</span>
-        </h3>
-      </div>
-
-      <Products />
-    </Container>
-  );
+  return <FadeCarousel />;
 }
