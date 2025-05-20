21ce0e3c3b7e6dcece73374580b24891250c7a8f
Giorgi Chapidze <chapidzeee.giorgi@gmail.com>
2025-05-20 14:12:07 +0400

    feat: integrate @cloudinary/react and @cloudinary/url-gen; remove local slide images from public/slides

--- src/features/carousel/FadeCarousel.tsx
+++ src/features/carousel/FadeCarousel.tsx
@@ -1,40 +1,41 @@
 import { Carousel } from "react-bootstrap";
+import { AdvancedImage } from "@cloudinary/react";
+import { CloudinaryImage } from "@cloudinary/url-gen";
+import { auto } from "@cloudinary/url-gen/actions/resize";
+import { slides } from "./slides";
+
 import "./FadeCarousel.css";
 
 export default function FadeCarousel() {
   return (
     <Carousel interval={3000} className="carousel">
-      {[1, 2, 3, 4, 5].map((i, index) => (
-        <Carousel.Item key={index}>
-          <picture>
-            {/* Small screens - portrait images */}
-            <source
-              srcSet={`/slides/${i}-540.jpg 540w, 
-                      /slides/${i}-810.jpg 810w, 
-                      /slides/${i}-1080.jpg 1080w`}
-              media="(max-width: 768px)"
-            />
-            {/* Larger screens - landscape images */}
-            <source
-              srcSet={`/slides/${i}-1440.jpg 1440w, 
-                      /slides/${i}-1920.jpg 1920w, 
-                      /slides/${i}-2400.jpg 2400w, 
-                      /slides/${i}-2880.jpg 2880w`}
-              media="(min-width: 769px)"
-            />
-            {/* Fallback image for older browsers or default */}
-            <img
-              src={`/slides/${i}-2400.jpg`}
-              srcSet={`/slides/${i}-1440.jpg 1440w,
-                       /slides/${i}-1920.jpg 1920w, 
-                       /slides/${i}-2400.jpg 2400w, 
-                       /slides/${i}-2880.jpg 2880w`}
-              sizes="100vw"
-              alt={`Slide ${i}`}
-            />
-          </picture>
-        </Carousel.Item>
-      ))}
+      {slides.map(({ id, desktop, mobile }) => {
+        const mobileImg = new CloudinaryImage(mobile.split("/upload/")[1], {
+          cloudName: "de57eqbvb",
+        }).resize(auto().width("auto"));
+
+        const desktopImg = new CloudinaryImage(desktop.split("/upload/")[1], {
+          cloudName: "de57eqbvb",
+        }).resize(auto().width("auto"));
+
+        return (
+          <Carousel.Item key={id}>
+            <picture>
+              <source media="(max-width: 768px)" srcSet={mobileImg.toURL()} />
+              <source media="(min-width: 769px)" srcSet={desktopImg.toURL()} />
+              <AdvancedImage
+                cldImg={desktopImg}
+                alt={`Slide ${id}`}
+                style={{
+                  width: "100%",
+                  height: "100%",
+                  objectFit: "cover",
+                }}
+              />
+            </picture>
+          </Carousel.Item>
+        );
+      })}
     </Carousel>
   );
 }
